c3cf9d05c2bf59b2441be856388262eaf3615233
Rasujon3 <rasujon3@gmail.com>
2023-01-21 01:30:54 +0600

    60-7 Implement Right Side Component and react ions

--- src/pages/Shared/RighttSideNav/RighttSideNav.js
+++ src/pages/Shared/RighttSideNav/RighttSideNav.js
@@ -1,9 +1,54 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaTrademark,
+  FaTwitch,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
 
 const RighttSideNav = () => {
   return (
     <div>
-      <h2>RighttSideNav</h2>
+      <ButtonGroup vertical>
+        <Button className="mb-2" variant="outline-primary">
+          {" "}
+          <FaGoogle></FaGoogle> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          {" "}
+          <FaGithub></FaGithub> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-4">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> WhatsApp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch /> Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTrademark /> Terms & Conditions{" "}
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel />
+      </div>
     </div>
   );
 };
